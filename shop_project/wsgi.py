18eeb9396adb95203d3bf8d96ef542188c3274d3
lebeaubm <lebeaubm@yahoo.com>
2024-07-29 06:25:21 -0700

    round 2

--- shop_project/wsgi.py
+++ shop_project/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-import dotenv
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_project.settings')
 
